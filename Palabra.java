2fc1f75c89b3411a1e77a958d5fa33713bcaa267
Bruno Schalch <brunoschalch@gmail.com>
2016-04-21 21:22:39 -0500

    Crear Palabra.java
    Cree el objeto Palabra con constructor y estado.

---
+++ Palabra.java
@@ -0,0 +1,23 @@
+public class Palabra {
+
+	final private String valor;
+	private int x;
+	private int y;
+	private int w;
+	private int h;
+	private int frecuencia;
+
+
+	public Palabra(String valor, int x, int y, int w, int h) {
+		this.valor = valor;
+		this.x=x;
+		this.y=y;
+		this.w=w;
+		this.h=h;
+		frecuencia=1;
+	}
+
+
+
+
+}
