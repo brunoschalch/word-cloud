957f55353de34c2345affd772943b65c66821568
anasollanok <anasollanokim@gmail.com>
2016-04-25 11:52:06 -0500

    Método de frecuencia
    Cuántas veces se ha repetido una palabra en el archivo de texto que se le pasa al método.

--- Palabra.java
+++ Palabra.java
@@ -5,7 +5,6 @@ public class Palabra {
 	private int y;
 	private int w;
 	private int h;
-	private int frecuencia;
 
 
 	public Palabra(String valor, int x, int y, int w, int h) {
@@ -14,10 +13,36 @@ public class Palabra {
 		this.y=y;
 		this.w=w;
 		this.h=h;
-		frecuencia=1;
 	}
 
+	public int frecuencia(File archivo){
+		
+		int cont = 1;
+		
+		try{
+			FileReader fileReader = new FileReader(archivo);
+			BufferedReader reader = new BufferedReader(fileReader);
+			String line = null;
+			
+			while((line = reader.readLine()) != null){
+			
+				String[] p = line.split(" "); //separa palabra por palabra y las guarda en el arreglo palabras
+			
+				for(int i=0; i<p.length; i++){
+				
+					if(valor.equals(p[i]))
+						cont++;
+				}
+			}
+		}
+		catch(IOException ioe){
+			ioe.printStackTrace();
+		}
+		
+		return cont;
+	}
 
+}
 
 
 }
