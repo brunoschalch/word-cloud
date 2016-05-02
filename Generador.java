e9fbad5f4e54533d976d2ffa5d3cd85aed391ba6
anasollanok <anasollanokim@gmail.com>
2016-05-01 22:48:06 -0500

    update
    con algunos comentarios referentes a detalles del diagrama

---
+++ Generador.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public abstract class Generador implements Graficable{
+
+	protected Palabra[] palabras;
+	protected String[] listaNegra;
+	protected String texto;
+	protected int limite;
+	
+	//signos en variables
+	//son public?
+	//falta el void de el metodo?
+	//falta el limite?
+	
+	public abstract void iniciar(File texto, File listaNegra, int limite);
+	
+	public abstract void llenarArreglo();
+	
+	/*public Scene crearNube(){
+	}*/
+}
