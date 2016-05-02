5669f228236011d5d3e7c829ed80726cbb0cda01
anasollanok <anasollanokim@gmail.com>
2016-05-01 22:51:41 -0500

    creación de Generador2

---
+++ Generador1.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Generador2 extends Generador{
+
+	//ya tengo listaNegra porque la heredé?
+	
+	public void iniciar(File texto, File listaNegra, int limite){
+	}
+	
+	public void llenarArreglo(){
+	}
+}
