447ce3faaba4a989f3c46dbe8ce750b3996a47df
Bruno Schalch <brunoschalch@gmail.com>
2016-05-09 15:01:32 -0500

    Cambios en el método llenarArreglo de Generador1.

--- Generador1.java
+++ Generador1.java
@@ -18,21 +18,28 @@ public class Generador1 extends Generador{
 		//importante ordenar el arreglo de palabra mas usada a menos usada
 		String[] textoSeparado = texto.toLowerCase().split("\\W+"); //pasa todo a minusculas y despues busca todas las no palabras con W y con + junta los que estan seguidos para separador, ver http://regexr.com/3dcpk
 
-			if (palabras!=null) {
-				for (String buscar : textoSeparado) {
-					boolean yaExiste = false;
-					for (Palabra palabraActual : palabras) {
-						if (palabraActual.getContenido().equals(buscar)) {
-							yaExiste=true;
-							palabraActual.actualizarFrecuencia();
-						}
-					}
-					if (!yaExiste) {
-						//no se encontro la palabra (String buscar) en el gran arreglo de palabras entonces hay que crearla
-						agregarPalabra(buscar);
-					}
+		if (palabras==null) {
+			//agregar el primer elemento manualmente
+			Label etiqueta = new Label(textoSeparado[0]);
+			palabras = new Palabra[1];
+			palabras[0] = new Palabra(textoSeparado[0], etiqueta);
+		}
+
+		for (int i=1; i<textoSeparado.length; i++) { //importante el uno para no contar la primera palabra dos veces
+			String buscar= textoSeparado[i];
+			boolean yaExiste = false;
+			for (Palabra palabraActual : palabras) {
+				if (palabraActual.getContenido().equals(buscar)) {
+					yaExiste=true;
+					palabraActual.actualizarFrecuencia();
 				}
 			}
+			if (!yaExiste) {
+				//no se encontro la palabra (String buscar) en el gran arreglo de palabras entonces hay que crearla
+				agregarPalabra(buscar);
+			}
+		}
+
 
 		ordenarArreglo();
 
--- Generador2.java
+++ Generador2.java
@@ -13,6 +13,45 @@ public class Generador2 extends Generador{
 	@Override
 	public void llenarArreglo(){ //aqui tambien se van generando las labels y se guardan en el estado de cada palabra, gracias a la agregacion se pueden recuperar con getLabel y usar en el UI en esta clase
 		//importante ordenar el arreglo de palabra mas usada a menos usada
+		String[] textoSeparado = texto.toLowerCase().split("\\W+"); //pasa todo a minusculas y despues busca todas las no palabras con W y con + junta los que estan seguidos para separador, ver http://regexr.com/3dcpk
+
+		if (palabras==null) {
+			//agregar el primer elemento manualmente
+			Label etiqueta = new Label(textoSeparado[0]);
+			palabras = new Palabra[1];
+			palabras[0] = new Palabra(textoSeparado[0], etiqueta);
+		}
+
+			for (int i=1; i<textoSeparado.length; i++) { //importante el uno para no contar la primera palabra dos veces
+				String buscar= textoSeparado[i];
+				if (!checarListaNegra(buscar)) { //solo se hace la operacion por cada palabra si no esta en la lista negra
+					boolean yaExiste = false;
+					for (Palabra palabraActual : palabras) {
+						if (palabraActual.getContenido().equals(buscar)) {
+							yaExiste = true;
+							palabraActual.actualizarFrecuencia();
+						}
+					}
+					if (!yaExiste) {
+						//no se encontro la palabra (String buscar) en el gran arreglo de palabras entonces hay que crearla
+						agregarPalabra(buscar);
+					}
+				}
+			}
+		}
+
+		ordenarArreglo();
+
+	}
+
+	public boolean checarListaNegra(String buscar) {
+		boolean coincidencia=false;
+		for (String palabra : listaNegra) {
+			if(palabra.equals(buscar)) {
+				coincidencia=true;
+			}
+		}
+		return coincidencia;
 	}
 
 }
