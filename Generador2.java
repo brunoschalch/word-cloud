22f89c0578ff8f69352e6f3f727a587485aa27f0
Bruno Schalch <brunoschalch@gmail.com>
2016-05-13 02:26:57 -0500

    Pequeños cambios.

--- Generador2.java
+++ Generador2.java
@@ -32,7 +32,7 @@ public class Generador2 extends Generador{
 						if (palabraActual.getContenido().equals(buscar)) {
 							yaExiste = true;
 							palabraActual.actualizarFrecuencia();
-							System.out.println("se actualizo"+ palabraActual.getContenido());
+						//	System.out.println("se actualizo"+ palabraActual.getContenido());
 						}
 					}
 					if (!yaExiste) {
